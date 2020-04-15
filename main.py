83db34f49af574a7c806e50fd6889e5d5b2552c2
andragabrielapufu99 <62243700+andragabrielapufu99@users.noreply.github.com>
2020-04-15 14:32:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from City import City
 from GA import GA
+def readData():
+    listOfCities = []
+    file = open("berlin52.txt","r")
+    content =  file.read().strip().split("\n")
+    index = 0
+    for i in range(6,len(content)-1):
+        params = content[i].split(" ")
+        nameCity = params[0]
+        xCoordCity = float(params[1])
+        yCoordCity = float(params[2])
+        city = City(index,nameCity,xCoordCity,yCoordCity)
+        listOfCities.append(city)
+        index += 1
+    file.close()
+    return listOfCities
 def readMatrix(filename):
     try:
         file = open(filename, "r")
@@ -18,84 +34,26 @@ def readMatrix(filename):
         raise ie
 
 def main():
-    try:
-        gaParam = {}  # dictionar pt parametrii algoritmului genetic
-        gaParam['dimensiunePopulatie'] = 10
-        gaParam['numarGeneratii'] = 100
+    gaParam = {}  # dictionar pt parametrii algoritmului genetic
+    gaParam['dimensiunePopulatie'] = 250
+    gaParam['numarGeneratii'] = 1500
 
-        probParam = {}  # dictionar pt parametrii problemei
-        print("File : matrix.txt")
-        matrix = readMatrix("matrix.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga1 = GA(gaParam,probParam)
-        ga1.initializarePopulatie()
-        solution = ga1.generational()
-        print(solution)
+    probParam = {}  # dictionar pt parametrii problemei
+    cities = readData()
+    matrix =[]
+    for i in range(len(cities)):
+        row = []
+        for j in range(len(cities)):
+            d = cities[i].measureDistance(cities[j])
+            row.append(d)
+        matrix.append(row)
 
-        print("\nFile : easy_03_tsp.txt")
-        matrix1 = readMatrix("easy_03_tsp.txt")
-        probParam['matrix'] = matrix1
-        probParam['noNodes'] = len(matrix1)
-        ga2 = GA(gaParam, probParam)
-        ga2.initializarePopulatie()
-        solution = ga2.generational()
-        print(solution)
+    probParam['matrix'] = matrix
+    probParam['noNodes'] = len(matrix)
+    ga = GA(gaParam, probParam)
+    ga.initializarePopulatie()
+    solution = ga.generational()
+    print(solution)
 
-        print("\nFile : medium_01_tsp.txt")
-        matrix = readMatrix("medium_01_tsp.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga3 = GA(gaParam, probParam)
-        ga3.initializarePopulatie()
-        solution = ga3.generational()
-        print(solution)
 
-        print("\nFile : hard_01_tsp.txt")
-        matrix = readMatrix("hard_01_tsp.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga4 = GA(gaParam, probParam)
-        ga4.initializarePopulatie()
-        solution = ga4.generational()
-        print(solution)
-
-        print("\nFile : easy1.txt")
-        matrix = readMatrix("easy1.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga5 = GA(gaParam, probParam)
-        ga5.initializarePopulatie()
-        solution = ga5.generational()
-        print(solution)
-
-        print("\nFile : easy2.txt")
-        matrix = readMatrix("easy2.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga6 = GA(gaParam, probParam)
-        ga6.initializarePopulatie()
-        solution = ga6.generational()
-        print(solution)
-
-        print("\nFile : easy3.txt")
-        matrix = readMatrix("easy3.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga7 = GA(gaParam, probParam)
-        ga7.initializarePopulatie()
-        solution = ga7.generational()
-        print(solution)
-
-        print("\nFile : mediumF.txt")
-        matrix = readMatrix("mediumF.txt")
-        probParam['matrix'] = matrix
-        probParam['noNodes'] = len(matrix)
-        ga8 = GA(gaParam, probParam)
-        ga8.initializarePopulatie()
-        solution = ga8.generational()
-        print(solution)
-
-    except IOError as ie:
-        print(ie)
 main()
