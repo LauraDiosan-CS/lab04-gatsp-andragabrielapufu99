ceb1cfc02f4b7792f83e629e3fb57fe39e6abacc
andragabrielapufu99 <62243700+andragabrielapufu99@users.noreply.github.com>
2020-04-15 14:31:03 +0300

    Update GA.py

--- GA.py
+++ GA.py
@@ -1,6 +1,6 @@
 import random
 from Cromozom import Cromozom
-
+from matplotlib import pyplot as plt
 class GA:
     def __init__(self,paramGA,paramProblema):
         self.__populatie = []
@@ -28,82 +28,63 @@ class GA:
         return f
 
     def selectie(self):
-        #selectie proportionala (ruleta)
-        sorted(self.__populatie,key =lambda x:x.getFitness())
-        sumaFitness = 0
-        n = len(self.__populatie)
-        for i in range(n):
-            sumaFitness += self.__populatie[i].getFitness()
-        ruleta = []
-        aux = 0
-        for i in range(n):
-            aux += self.__populatie[i].getFitness()/sumaFitness
-            ruleta.append(aux)
-        alegere = random.uniform(ruleta[0],ruleta[len(ruleta)-1])
-
-        for i in range(len(ruleta)):
-            if alegere<ruleta[i]:
-                return self.__populatie[i]
+        #selectie turnir
+        limita = len(self.__populatie) * 20// 100
+        vmax = 1000000
+        index = 0
+        for i in range(limita):
+            if self.__populatie[i].getFitness() < vmax:
+                vmax = self.__populatie[i].getFitness()
+                index = i
+        return self.__populatie[index]
 
     def incrucisare(self,mother,father):
         #incrucisare ordonata
 
-        child1 = [-1 for _ in range(self.__paramProblema['noNodes'])] #genotip descendent1
-        child2 = [-1 for _ in range(self.__paramProblema['noNodes'])] #genotip descendent2
+        pos1 = random.randint(-1, self.__paramProblema['noNodes']-1)
+        pos2 = random.randint(-1, self.__paramProblema['noNodes']-1)
 
-        #g1,g2 pozitiile subsirului ales
-        g1 = random.randint(0, self.__paramProblema['noNodes']-1)
-        g2 = random.randint(0, self.__paramProblema['noNodes']-1)
-        while g1 == g2:
-            g2 = random.randint(0, self.__paramProblema['noNodes']-1)
+        if (pos2 < pos1):
+            aux = pos1
+            pos1 = pos2
+            pos2 = aux
 
-        if g1>g2:
-            aux=g1
-            g1=g2
-            g2=aux
+        k = 0
+        newrepres1 = mother.getReprezentare()[pos1: pos2]
+        for el in mother.getReprezentare()[pos2:] + mother.getReprezentare()[:pos2]:
+            if (el not in newrepres1):
+                if (len(newrepres1) < self.__paramProblema['noNodes'] - pos1):
+                    newrepres1.append(el)
+                else:
+                    newrepres1.insert(k, el)
+                    k += 1
+        descendent1 = Cromozom(newrepres1,self.fitnessFunction(newrepres1))
 
-        for i in range(g1,g2+1):
-            child1[i] = mother.getReprezentare()[i]
-            child2[i] = father.getReprezentare()[i]
-
-        for i in range(g2+1,len(child1)):
-            if father.getReprezentare()[i] not in child1:
-                child1[i] = father.getReprezentare()[i]
-            if mother.getReprezentare()[i] not in child2:
-                child2[i] = mother.getReprezentare()[i]
-        index = 0
-        for i in range(len(child1)):
-            if father.getReprezentare()[i] not in child1 and -1 in child1:
-                while child1[index]!=-1:
-                    index += 1
-                child1[index]=father.getReprezentare()[i]
-        index = 0
-        for i in range(len(child2)):
-            if mother.getReprezentare()[i] not in child2 and -1 in child2:
-                while child2[index]!=-1:
-                    index += 1
-                child2[index] = mother.getReprezentare()[i]
-
-        fitnessChild1 = self.fitnessFunction(child1)
-        fitnessChild2 = self.fitnessFunction(child2)
-
-        descendent1 = Cromozom(child1,fitnessChild1)
-        descendent2 = Cromozom(child2,fitnessChild2)
-
+        newrepres2 = father.getReprezentare()[pos1: pos2]
+        for el in father.getReprezentare()[pos2:] + father.getReprezentare()[:pos2]:
+            if (el not in newrepres2):
+                if (len(newrepres2) < self.__paramProblema['noNodes'] - pos1):
+                    newrepres2.append(el)
+                else:
+                    newrepres2.insert(k, el)
+                    k += 1
+        descendent2 = Cromozom(newrepres2,self.fitnessFunction(newrepres2))
         return descendent1,descendent2
 
     def mutatie(self,individ):
-        # mutatie prin interschimbare
-        newReprezentare = []
-        g1 = random.randint(0,len(individ.getReprezentare())-1)
-        g2 = random.randint(0,len(individ.getReprezentare())-1)
-        while g1==g2:
-            g2 = random.randint(0, len(individ.getReprezentare())-1)
-        r = individ.getReprezentare()
-        aux = r[g1]
-        r[g1] = r[g2]
-        r[g2] = aux
-        individ.setReprezentare(r)
+        # mutatie prin insertie
+        pos1 = random.randint(0, len(individ.getReprezentare())-1)
+        pos2 = random.randint(0, len(individ.getReprezentare())-1)
+        if (pos2 < pos1):
+            aux = pos1
+            pos1 = pos2
+            pos2 = aux
+        newReprezentare = individ.getReprezentare()
+        el = newReprezentare[pos2]
+        del newReprezentare[pos2]
+        newReprezentare.insert(pos1 + 1, el)
+        individ.setReprezentare(newReprezentare)
+        individ.setFitness(self.fitnessFunction(newReprezentare))
         return individ
 
     def getPopulatie(self):
@@ -119,13 +100,15 @@ class GA:
         return self.__populatie[index]
 
     def generational(self):
+
         bestFitness = self.getBestCromozom().getFitness()
+        allFitness = [bestFitness]
         print("Best fitness : " +str(bestFitness))
         print("Best repres : "+str(self.getBestCromozom().getReprezentare()))
         print()
         for i in range(self.__paramGA['numarGeneratii']):
             populatieNoua = [self.getBestCromozom()]
-            for j in range(self.__paramGA['dimensiunePopulatie']-1):
+            while (len(populatieNoua)!=self.__paramGA['dimensiunePopulatie']):
                 # selectez 2 parinti
                 mother = self.selectie()
                 father = self.selectie()
@@ -151,15 +134,18 @@ class GA:
                 if bestOne.getFitness() > father.getFitness():
                     if father not in populatieNoua:
                         bestOne = father
-
                 populatieNoua.append(bestOne)
 
             self.__populatie = populatieNoua
 
             b = self.getBestCromozom().getFitness()
+            allFitness.append(b)
             if b!=bestFitness:
                 bestFitness = b
-                print("Best fitness : " +str(bestFitness))
+                print("Best fitness : " + str(bestFitness))
                 print("Best repres : " + str(self.getBestCromozom().getReprezentare()))
                 print()
+
+        plt.plot(allFitness, '-b', label="TSP GA Fitness")
+        plt.show()
         return self.getBestCromozom()
